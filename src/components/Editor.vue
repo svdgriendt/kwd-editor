18eac1473aca5981fba720b5e5dbf824cb2678f8
Stefan van de Griendt <svdgriendt@users.noreply.github.com>
2022-09-08 00:10:00 +0200

    Add basic tiptap editor

---
+++ src/components/Editor.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import { useEditor, EditorContent } from '@tiptap/vue-3';
+  import StarterKit from '@tiptap/starter-kit';
+  
+  const tiptap = useEditor({
+    extensions: [StarterKit]
+  });
+  </script>
+  
+  <template>
+    <editor-content :editor="tiptap" />
+  </template>
