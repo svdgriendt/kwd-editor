9be80a95f8fbdadaa7701fa772daaa524f3ae9b4
Stefan van de Griendt <svdgriendt@users.noreply.github.com>
2022-09-16 23:22:52 +0200

    Add basic marks bold, italic and strike

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-  import { useEditor, EditorContent } from '@tiptap/vue-3';
-  import StarterKit from '@tiptap/starter-kit';
+import { useEditor, EditorContent } from '@tiptap/vue-3';
+import StarterKit from '@tiptap/starter-kit';
+import EditorMenuBar from './EditorMenuBar.vue';
+
+const tiptap = useEditor({
+  extensions: [StarterKit]
+});
+</script>
   
-  const tiptap = useEditor({
-    extensions: [StarterKit]
-  });
-  </script>
-  
-  <template>
-    <editor-content :editor="tiptap" />
-  </template>
+<template>
+  <editor-menu-bar :editor="tiptap!" />
+  <editor-content :editor="tiptap" />
+</template>
---
+++ src/components/EditorMenuBar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Editor } from '@tiptap/vue-3';
+
+const props = defineProps<{ editor: Editor }>();
+
+function toggleMark(mark: 'Bold' | 'Italic' | 'Strike') {
+  props.editor.chain().focus()[`toggle${mark}`]().run();
+}
+function isMarkActive(style: 'bold' | 'italic' | 'strike') {
+  return props.editor?.isActive(style);
+}
+</script>
+  
+<template>
+  <button @click="toggleMark('Bold')" :class="{active: isMarkActive('bold')}"><i class="fa-solid fa-bold"></i></button>
+  <button @click="toggleMark('Italic')" :class="{active: isMarkActive('italic')}"><i class="fa-solid fa-italic"></i></button>
+  <button @click="toggleMark('Strike')" :class="{active: isMarkActive('strike')}"><i class="fa-solid fa-strikethrough"></i></button>
+</template>
