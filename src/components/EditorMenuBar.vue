5df89f50d1fb6fd46e772298f7867a148e6884ef
Stefan van de Griendt <svdgriendt@users.noreply.github.com>
2022-09-18 22:42:41 +0200

    Add support for headings

--- src/components/EditorMenuBar.vue
+++ src/components/EditorMenuBar.vue
@@ -10,11 +10,18 @@ const marks = {
   underline: 'underline',
   strike: 'strikethrough'
 };
+const headings = {
+  1: '1',
+  2: '2',
+  3: '3',
+  4: '4'
+};
 
 type KeysOfType<T> = {
   [K in keyof T]: K;
 }[keyof T];
 type Marks = KeysOfType<typeof marks>;
+type Headings = KeysOfType<typeof headings>;
 
 function toggleMark(mark: Marks) {
   const capitalized = mark[0].toUpperCase() + mark.slice(1) as Capitalize<Marks>;
@@ -24,10 +31,22 @@ function toggleMark(mark: Marks) {
 function isMarkActive(style: Marks) {
   return props.editor?.isActive(style);
 }
+
+function toggleHeading(level: Headings) {
+  props.editor.chain().focus().toggleHeading({ level }).run();
+}
+
+function isHeadingActive(level: Headings) {
+  return props.editor?.isActive('heading', { level });
+}
 </script>
   
 <template>
   <button v-for="mark in Object.entries(marks)" :key="mark[0]" @click="toggleMark(mark[0] as Marks)" :class="{active: isMarkActive(mark[0] as Marks)}">
     <i :class="`fa-solid fa-${mark[1]}`"></i>
+  </button>
+
+  <button v-for="heading in Object.entries(headings)" :key="heading[0]" @click="toggleHeading(parseInt(heading[0]) as Headings)" :class="{active: isHeadingActive(parseInt(heading[0]) as Headings)}">
+    <i class="fa-solid fa-heading"></i> <i :class="`fa-solid fa-${heading[1]}`"></i>
   </button>
 </template>
